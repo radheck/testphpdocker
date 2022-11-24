18491f670ce6cf6bbf5b20d3a75da32b527b95da
radheck <radheck2@gmail.com>
2022-11-24 12:48:04 +0100

    Update index.php

--- data/web1/index.php
+++ data/web1/index.php
@@ -20,7 +20,7 @@ if ($result->num_rows > 0) {
         echo "País paco: " . $row["name"]. " - Continente: " . $row["continent"]. " -Región: " . $row["region"]. "<br>";
     }
 } else {
-    echo "0 results juanjo";
+    echo "0 results NILO TEST";
 }
 $conn->close();
 ?>
